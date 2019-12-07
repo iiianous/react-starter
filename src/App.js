0dd4536a5f856e98261cea909b8d5240fd4bd7d3
Ian Mostar <ian@rallyhr.com>
2019-12-07 17:05:24 +0800

    Update files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => (
-    <h1>Hello, React!</h1>
-)
+const ToDo = props => {
+    let { title, id } = props.todo
+
+    return (
+        <div className="todo-list-item" value={id}>
+            {title}
+        </div>
+    )
+}
+
+const App = () => {
+    const [todos, setTodos] = useState([
+        {
+            title: 'Learn React',
+            id: 111
+        },
+        {
+            title: 'Learn Vue',
+            id: 222
+        },
+        {
+            title: 'Learn Node.js',
+            id: 333
+        }
+    ]);
+
+    return (
+        <div className="app">
+            <div className="todo-list">
+                <h1>Hello React.js</h1>
+                {
+                    todos.map( (todo, index) => (
+                        <ToDo todo={todo} index={index} key={index}></ToDo>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
 
 export default App
